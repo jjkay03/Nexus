51c72251586cfee0dbf727d31fd3479a6820d6d5
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-12-29 19:19:33 +0100

    Companion object messages paths

--- src/main/kotlin/com/jjkay03/nexus/commands/NexusCommand.kt
+++ src/main/kotlin/com/jjkay03/nexus/commands/NexusCommand.kt
@@ -1,6 +1,7 @@
 package com.jjkay03.nexus.commands
 
 import com.jjkay03.nexus.utility.MessagesManager
+import com.jjkay03.nexus.utility.MessagesPath
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -11,7 +12,7 @@ class NexusCommand: CommandExecutor {
     // Command
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player) {
-            sender.sendMessage(MessagesManager.getMessage("error.not-player-command"))
+            sender.sendMessage(MessagesManager.getMessage(MessagesPath.ERROR_NOT_PLAYER_COMMAND))
         }
 
         return true
