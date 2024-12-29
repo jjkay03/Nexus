4ebdf75b41d20c4045aa2e9b1d71e80080ed171f
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-12-29 15:18:53 +0100

    Initial commit

---
+++ src/main/kotlin/com/jjkay03/nexus/Nexus.kt
@@ -0,0 +1,14 @@
+package com.jjkay03.nexus
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Nexus : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
