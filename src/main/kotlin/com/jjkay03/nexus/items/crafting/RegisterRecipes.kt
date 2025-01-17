85d44346aa27f3b0569a8a117218bb6622441d7e
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2025-01-17 20:23:22 +0100

    Updated recipes register

--- src/main/kotlin/com/jjkay03/nexus/items/crafting/RegisterRecipes.kt
+++ src/main/kotlin/com/jjkay03/nexus/items/crafting/RegisterRecipes.kt
@@ -1,13 +1,16 @@
 package com.jjkay03.nexus.items.crafting
 
+import com.jjkay03.nexus.Saves
 import com.jjkay03.nexus.items.NexusItems
 import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
 
 class RegisterRecipes {
     init {
         NexusItems.entries.forEach { nexusItem ->
-            val recipe = nexusItem.itemClass.recipe()
-            if (recipe != null) Bukkit.addRecipe(recipe)
+            val recipe = nexusItem.itemClass.recipe()?: return@forEach
+            Bukkit.removeRecipe(NamespacedKey(Saves.NEXUS_NAMESPACE, nexusItem.itemClass.id)) // Remove recipe (in case it exist)
+            Bukkit.addRecipe(recipe) // Register recipe
         }
     }
 }
